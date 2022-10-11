619faf888090cd43ea6adca5ad2885c493dbd5cf
Robert King <robertkng104@gmail.com>
2022-10-10 17:58:00 -0700

    Add error logging

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,6 +4,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=1G
 #SBATCH --partition=serc
+#SBATCH --error=error.err
 module load python/3.9.0
 # Recreate python env from scratch each run
 python3 -m venv $SCRATCH/uncert_quant/$SLURM_JOBID/env
@@ -11,8 +12,7 @@ source $SCRATCH/uncert_quant/$SLURM_JOBID/env/bin/activate
 python3 -m pip -r requirements.txt
 # Create run directory
 mkdir $SCRATCH/uncert_quant/$SLURM_JOBID
-mkdir $SCRATCH/uncert_quant/$SLURM_JOBID/logs
 # Run dispatcher
-python dispatcher.py $1 > $SCRATCH/uncert_quant/$SLURM_JOBID/dispatcher.out
+python dispatcher.py $@ > $SCRATCH/uncert_quant/$SLURM_JOBID/dispatcher.out
 deactivate 
 rm -rf $SCRATCH/uncert_quant/$SLURM_JOBID/env
