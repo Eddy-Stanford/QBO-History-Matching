30199397c114a315f5ab8a95eb8fca5478bc5ed0
Robert King <robertkng104@gmail.com>
2022-10-10 18:03:31 -0700

    Fix bad mkdir call

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,14 +4,14 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=1G
 #SBATCH --partition=serc
+# Create run directory
+mkdir $SCRATCH/uncert_quant/$SLURM_JOBID
 module load python/3.9.0
 # Recreate python env from scratch each run
 python3 -m venv $SCRATCH/uncert_quant/$SLURM_JOBID/env
 source $SCRATCH/uncert_quant/$SLURM_JOBID/env/bin/activate
 python3 -m pip install -r requirements.txt
-# Create run directory
-mkdir $SCRATCH/uncert_quant/$SLURM_JOBID
 # Run dispatcher
-python dispatcher.py $@ > $SCRATCH/uncert_quant/$SLURM_JOBID/dispatcher.out
+python dispatcher.py "$@" > $SCRATCH/uncert_quant/$SLURM_JOBID/dispatcher.out
 deactivate 
 rm -rf $SCRATCH/uncert_quant/$SLURM_JOBID/env
