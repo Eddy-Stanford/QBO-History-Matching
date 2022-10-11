2f3d0b2f80c4af03939e2746b40a291d7823a3f3
Robert King <robertkng104@gmail.com>
2022-10-10 17:59:43 -0700

    Fix bad env setup

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,12 +4,11 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=1G
 #SBATCH --partition=serc
-#SBATCH --error=error.err
 module load python/3.9.0
 # Recreate python env from scratch each run
 python3 -m venv $SCRATCH/uncert_quant/$SLURM_JOBID/env
 source $SCRATCH/uncert_quant/$SLURM_JOBID/env/bin/activate
-python3 -m pip -r requirements.txt
+python3 -m pip install -r requirements.txt
 # Create run directory
 mkdir $SCRATCH/uncert_quant/$SLURM_JOBID
 # Run dispatcher
