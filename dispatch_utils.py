d9ea0c915d8fc5ea29aea1fd39a27de4febd7683
Robert King <robcking@stanford.edu>
2023-01-27 11:25:49 -0800

    add script to submit round 2

---
+++ dispatch_utils.py
@@ -0,0 +1,62 @@
+import argparse
+from pathlib import Path
+import re
+import os
+import shutil
+import subprocess
+import jinja2
+if 'SLURM_JOBID' not in os.environ:
+    raise RuntimeError("This script can only be accessed from a SLURM job")
+
+
+CURRENT_CW = 35
+CURRENT_BT= 0.0043
+
+HOME_EXECUTABLE = Path.home()/'MiMA'/'exp'/'exec.SH03_CEES'/'mima.x'
+INPUT_FILES = Path.home()/'MiMA'/'input'
+
+
+def model_run(basedir,nruns,t,concurrency=20):
+    return subprocess.run(['sbatch',
+        '--chdir',basedir,
+        '--array',f'0-{nruns-1}%{concurrency}'
+        'model_run.sh'
+        ,str(t),
+    ]) 
+
+
+def get_template(name='input.nml.template'):
+    template_loader = jinja2.FileSystemLoader(searchpath="./")
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template('input.nml.template')
+    return template
+
+def write_namefile(dir,template,cw,Bt):
+    with open(os.path.join(dir,'input.nml'),'w') as input_namefile:
+        input_namefile.write(template.render(cw=cw,Bt_eq=Bt))
+
+def get_base_dir(expname:str):
+    if expname:
+        return os.path.expandvars(f'$SCRATCH/uncert_quant/{expname}')
+    else:
+        return os.path.expandvars('$SCRATCH/uncert_quant/$SLURM_JOBID')
+
+def positive_int(val):
+    val = int(val)
+    if val <= 0:
+        raise argparse.ArgumentTypeError('Value must be positive')
+    return val
+
+def create_run_dirs(base:str,run_id:int)->str:
+    run_dir =os.path.join(base,str(run_id).zfill(2))
+    os.makedirs(run_dir)
+    ## Copy Executable
+    shutil.copy(HOME_EXECUTABLE,os.path.join(run_dir,'mima.x'))
+    ## Copy Input files
+    shutil.copytree(INPUT_FILES,os.path.join(run_dir),dirs_exist_ok=True)
+    ## Make RESTART dir
+    os.makedirs(os.path.join(run_dir,'RESTART'))
+    return run_dir
+
+def get_job_number(stdout:str)->int:
+    return int(re.match(r'[a-zA-Z]*([0-9]+)',stdout).group(1))
--- dispatcher.py
+++ dispatcher.py
@@ -1,65 +1,21 @@
-from typing import List
 import numpy as np
 import argparse
 import os
-import subprocess
-import jinja2
-import re
-import shutil
 import csv
-from pathlib import Path
 from pyDOE import lhs
-
-if 'SLURM_JOBID' not in os.environ:
-    raise RuntimeError("This script can only be accessed from a SLURM job")
-
-CURRENT_CW = 35
-CURRENT_BT= 0.0043
-
-HOME_EXECUTABLE = Path.home()/'MiMA'/'exp'/'exec.SH03_CEES'/'mima.x'
-INPUT_FILES = Path.home()/'MiMA'/'input'
-
-def get_base_dir(expname):
-    if expname:
-        return os.path.expandvars(f'$SCRATCH/uncert_quant/{expname}')
-    else:
-        return os.path.expandvars('$SCRATCH/uncert_quant/$SLURM_JOBID')
-
-def positive_int(val):
-    val = int(val)
-    if val <= 0:
-        raise argparse.ArgumentTypeError('Value must be positive')
-    return val
-
-def create_run_dirs(base:str,run_id:int)->str:
-    run_dir =os.path.join(base,str(run_id).zfill(2))
-    os.makedirs(run_dir)
-    ## Copy Executable
-    shutil.copy(HOME_EXECUTABLE,os.path.join(run_dir,'mima.x'))
-    ## Copy Input files
-    shutil.copytree(INPUT_FILES,os.path.join(run_dir),dirs_exist_ok=True)
-    ## Make RESTART dir
-    os.makedirs(os.path.join(run_dir,'RESTART'))
-    return run_dir
-
-def get_job_number(stdout:str)->int:
-    return int(re.match(r'[a-zA-Z]*([0-9]+)',stdout).group(1))
+import dispatch_utils
 
 parser = argparse.ArgumentParser(description='Run Uncertanity Quantification experiments')
-parser.add_argument('n_years',default=20,type=positive_int)
-parser.add_argument('n_runs',default=20,type=positive_int)
+parser.add_argument('n_years',default=20,type=dispatch_utils.positive_int)
+parser.add_argument('n_runs',default=20,type=dispatch_utils.positive_int)
 parser.add_argument('--seed',default=None,type=int)
 parser.add_argument('--rng-offset',default=0,type=int)
 parser.add_argument('--exp-name',default=None,type=str)
 
-
 if __name__ == '__main__':
     args = parser.parse_args()
-    base = get_base_dir(args.exp_name)
-    template_loader = jinja2.FileSystemLoader(searchpath="./")
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template('input.nml.template')
-
+    base = dispatch_utils.get_base_dir(args.exp_name)
+    template = dispatch_utils.get_template()
     samples = lhs(2,samples=args.n_runs) 
     samples = np.column_stack((samples,np.ones((args.n_runs,))))
     rescale = np.array([[60,0],[0,0.005],[5,0.001]])
@@ -68,10 +24,9 @@ if __name__ == '__main__':
     for run in range(args.n_runs):
         run_cw = samples[run,0]
         run_Bt = samples[run,1]
-        run_dir = create_run_dirs(base,run)
-
-        with open(os.path.join(run_dir,'input.nml'),'w') as input_namefile:
-            input_namefile.write(template.render(cw=run_cw,Bt_eq=run_Bt))
+        run_dir = dispatch_utils.create_run_dirs(base,run)
+        dispatch_utils.write_namefile(run_dir,template,run_cw,run_Bt)
+    
     
     with open(os.path.join(base,'paramlist.csv'),'w',newline='\n') as f:
         paramlist = csv.writer(f, delimiter=',',
@@ -79,9 +34,4 @@ if __name__ == '__main__':
         paramlist.writerow(['run_id','cw','Bt'])
         paramlist.writerows([[i,str(samples[i,0]),str(samples[i,1])] for i in range(args.n_runs)])
 
-    subprocess.run(['sbatch',
-    '--chdir',base,
-    '--array',f'0-{args.n_runs-1}%20'
-    'model_run.sh'
-    ,str(args.n_years),
-    ]) 
+    dispatch_utils.model_run()
