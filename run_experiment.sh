ee19ca44e2e25026aefc49f51e7a21fd5b94c339
Robert King <robcking@stanford.edu>
2023-01-27 11:53:27 -0800

    Switch to using expname instead of using JOBID

--- run_experiment.sh
+++ run_experiment.sh
@@ -5,13 +5,24 @@
 #SBATCH --mem=1G
 #SBATCH --partition=serc
 # Create run directory
-mkdir $SCRATCH/uncert_quant/$SLURM_JOBID
+for ((i=1; i<=$#;i++));
+do
+    if [ ${!i} = "--exp-name" ]
+    then ((i++))
+        expname=${!i};
+    fi
+done;
+if [ -z "$expname" ];
+then
+    expname=$SLURM_JOBID
+fi;
+mkdir $SCRATCH/uncert_quant/$expname
 module load python/3.9.0
 # Recreate python env from scratch each run
-python3 -m venv $SCRATCH/uncert_quant/$SLURM_JOBID/env
-source $SCRATCH/uncert_quant/$SLURM_JOBID/env/bin/activate
+python3 -m venv $SCRATCH/uncert_quant/$expname/env
+source $SCRATCH/uncert_quant/$expname/env/bin/activate
 python3 -m pip install -r requirements.txt
 # Run dispatcher
 python dispatcher.py "$@"
 deactivate 
-rm -rf $SCRATCH/uncert_quant/$SLURM_JOBID/env
+rm -rf $SCRATCH/uncert_quant/$expname/env
