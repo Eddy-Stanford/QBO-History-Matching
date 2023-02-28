d9cf306f94e9fc5d81cf0b5bcd624f0abcc25c78
Robert King <robcking@stanford.edu>
2023-02-27 19:16:07 -0800

    use base dir

--- experiment_init.py
+++ experiment_init.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import joblib
@@ -11,12 +12,12 @@ def get_first_wave_space(sample_space, **kwargs):
     calc_space = SampleSpace.from_bound_dict(bounds)
     return calc_space
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     configfile = sys.argv[1]
-    config = dispatch_utils.load_config_file(configfile,0)
+    config = dispatch_utils.load_config_file(configfile, 0)
+    base = dispatch_utils.get_exp_base_dir(**config)
     space = get_first_wave_space(**config)
-    initial_samples = space.lhs_sample(config['nruns_per_wave'],labelled=True)
-    initial_samples.to_csv('0_samples.csv')
-    joblib.dump(space,'0.space')
-
-
+    initial_samples = space.lhs_sample(config["nruns_per_wave"], labelled=True)
+    initial_samples.to_csv(os.path.join(base,"0_samples.csv"))
+    joblib.dump(space, os.path.join(base,"0.space"))
