99117b64bb2fc6921e320e483c33b1c8a1fc16ac
Robert King <robcking@stanford.edu>
2023-02-28 10:14:25 -0800

    oh dear

--- extract_qbo/bulk_extract_qbo.sh
+++ extract_qbo/bulk_extract_qbo.sh
@@ -7,6 +7,6 @@ module load python/3.9.0
 source $1/../env/bin/activate
 for i in {0..9}; do
     printf -v idx "%s02d"  $((SLURM_ARRAY_TASK_ID+i))
-    [ -d $i/$idx ] && srun -n 1 python extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 & 
+    [ -d $1/$idx ] && srun -n 1 python extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 & 
 done
 wait
