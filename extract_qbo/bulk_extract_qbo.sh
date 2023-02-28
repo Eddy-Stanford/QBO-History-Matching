00f55f9be4d2b29ed45d72adc1bb3ec23dd5025a
Robert King <robcking@stanford.edu>
2023-02-28 10:57:57 -0800

    i am not happy

--- extract_qbo/bulk_extract_qbo.sh
+++ extract_qbo/bulk_extract_qbo.sh
@@ -5,11 +5,10 @@
 #SBATCH --ntasks=10
 module load python/3.9.0
 source $1/../env/bin/activate
-scriptdir=$(dirname "${BASH_SOURCE[0]}")
 for i in {0..9}; do
     printf -v idx "%02d"  $((SLURM_ARRAY_TASK_ID+i))
     echo "working on " $1/$idx
-    [ -d $1/$idx ] && srun -n 1 python $scriptdir/extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 & 
+    [ -d $1/$idx ] && srun -n 1 python extract_qbo/extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 & 
 done
 wait
 echo "done"
