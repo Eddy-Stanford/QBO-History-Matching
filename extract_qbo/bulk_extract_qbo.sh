06b57d7ab3060fe932fef0c6e985c2cbe2462016
Robert King <robcking@stanford.edu>
2023-02-28 11:27:03 -0800

    swap order

--- extract_qbo/bulk_extract_qbo.sh
+++ extract_qbo/bulk_extract_qbo.sh
@@ -7,9 +7,9 @@ module load python/3.9.0
 source $1/../env/bin/activate
 for i in {0..9}; do
     printf -v idx "%02d"  $((SLURM_ARRAY_TASK_ID+i))
-    echo "working on " $1/$idx
     if [ -d $1/$idx ]
     then
+        echo "working on " $1/$idx
         srun -n 1 python extract_qbo/extract_qbo.py $1/$idx $2 $3 --output-name "${1}/${idx}_QBO_${2}_${3}.nc" --latitude_range=5 & 
     fi
 done
