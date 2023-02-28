d0dd76c3c91c4b70d19d02bb0f151746d8a24bbf
Robert King <robcking@stanford.edu>
2023-02-27 23:58:13 -0800

    use local python env throughout

--- extract_qbo/bulk_extract_qbo.sh
+++ extract_qbo/bulk_extract_qbo.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=serc
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=10
-source ~/.bashrc
-conda activate
+module load python/3.9.0
+source $1/../env/bin/activate
 for i in {0..9}; do
     printf -v idx "%s02d"  $((SLURM_ARRAY_TASK_ID+i))
     [ -d $i/$idx ] && srun -n 1 python extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 & 
