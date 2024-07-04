47e197cddb1490650b816c826e5970f1003ba348
Robert King <robcking@stanford.edu>
2024-07-03 20:51:50 -0700

    WIP: SLURM signalling

--- slurm_scripts/model_run.sh
+++ slurm_scripts/model_run.sh
@@ -6,8 +6,10 @@
 #SBATCH --output=%a.log
 #SBATCH -c 1
 #SBATCH --constraint=CPU_GEN:RME
-#SBATCH --mail-type=FAIL,TIME_LIMIT_90 
-set -e 
+#SBATCH --mail-type=FAIL,TIME_LIMIT_90
+#SBATCH --signal=B:SIGUSR1@120
+
+set -e
 iterations=$1
 printf -v idx "%02d" $SLURM_ARRAY_TASK_ID
 cd $idx
@@ -16,17 +18,27 @@ echo "Running for " $iterations " years"
 
 mimacontainer="${MIMAPATH:-/home/groups/aditis2/robcking/mima.sif}"
 
-for ((i=1;i<=$iterations;i++))
-do 
-    echo "Running year " $i
-    ## Implement backoff 
-    for j in {1..5}
-    do 
-        echo "attempt ${j}"
-         srun --ntasks $SLURM_NTASKS apptainer run $mimacontainer && break || sleep 15;
-    done
-    apptainer exec $mimacontainer /opt/MiMA/build/mppnccombine.singularity -r atmos_daily_${i}.nc atmos_daily.nc.????
+function cleanup(){
+    echo "TIME LIMIT IS CLOSE...cleaning up and requeueing"
+    
+    echo "Move RESTART FILES"
+}
+trap 'cleanup' SIGUSR1
 
+for ((i=1;i<=$iterations;i++))
+do
+    echo "Running year " $i
+    # ## Implement backoff
+    # for j in {1..5}
+    # do
+    #     echo "attempt ${j}"
+    #      srun --ntasks $SLURM_NTASKS apptainer run $mimacontainer && break || sleep 15;
+    # done
+    srun --ntasks $SLURM_NTASKS apptainer run $mimacontainer &
+    children=$!
+    wait
+    apptainer exec $mimacontainer /opt/MiMA/build/mppnccombine.singularity -r atmos_daily_${i}.nc atmos_daily.nc.????
+    
     cp RESTART/*res* INPUT/
     [ ! -d restart_history/restart_$i ] && mkdir -p restart_history/restart_$i
     cp -r RESTART/*res* restart_history/restart_$i/
