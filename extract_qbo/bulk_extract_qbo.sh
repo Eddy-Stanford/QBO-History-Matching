d8634b186b775a7a622d22a0942bc1ae32ff6cf5
Robert King <robcking@stanford.edu>
2024-07-26 12:05:18 -0700

    Update bulk_extract to use python3.12

--- extract_qbo/bulk_extract_qbo.sh
+++ extract_qbo/bulk_extract_qbo.sh
@@ -3,18 +3,18 @@
 #SBATCH --partition=serc
 #SBATCH --time=01:00:00
 #SBATCH -n 10
-#SBATCH -N 1 
-#SBATCH --mail-type=FAIL,TIME_LIMIT_90 
+#SBATCH -N 1
+#SBATCH --mail-type=FAIL,TIME_LIMIT_90
 
-module load python/3.9.0
+module load python/3.12
 source $1/../env/bin/activate
 for i in {0..9}; do
     printf -v idx "%02d"  $((SLURM_ARRAY_TASK_ID+i))
     if [ -d $1/$idx ]
     then
         echo "working on " $1/$idx
-        python extract_qbo/extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 & 
+        python extract_qbo/extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 &
     fi
 done
 wait
-echo "done" 
+echo "done"
--- slurm_scripts/model_run.sh
+++ slurm_scripts/model_run.sh
@@ -6,8 +6,8 @@
 #SBATCH --output=%a.log
 #SBATCH -c 1
 #SBATCH --constraint=CPU_GEN:RME
-#SBATCH --mail-type=FAIL,TIME_LIMIT_90 
-set -e 
+#SBATCH --mail-type=FAIL,TIME_LIMIT_90
+set -e
 iterations=$1
 printf -v idx "%02d" $SLURM_ARRAY_TASK_ID
 cd $idx
@@ -17,16 +17,16 @@ echo "Running for " $iterations " years"
 mimacontainer="${MIMAPATH:-/home/groups/aditis2/robcking/mima.sif}"
 
 for ((i=1;i<=$iterations;i++))
-do 
+do
     echo "Running year " $i
-    ## Implement backoff 
+    ## Implement backoff
     for j in {1..5}
-    do 
+    do
         echo "attempt ${j}"
-         srun --ntasks $SLURM_NTASKS apptainer run $mimacontainer && break || sleep 15;
+        srun --ntasks $SLURM_NTASKS apptainer run $mimacontainer && break || sleep 15;
     done
     apptainer exec $mimacontainer /opt/MiMA/build/mppnccombine.singularity -r atmos_daily_${i}.nc atmos_daily.nc.????
-
+    
     cp RESTART/*res* INPUT/
     [ ! -d restart_history/restart_$i ] && mkdir -p restart_history/restart_$i
     cp -r RESTART/*res* restart_history/restart_$i/
