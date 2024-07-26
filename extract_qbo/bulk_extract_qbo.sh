6c395fe12ddf17cb0778f3b9531afa169692d38f
Robert King <robcking@stanford.edu>
2024-07-26 12:32:04 -0700

    remove one node constraint

--- extract_qbo/bulk_extract_qbo.sh
+++ extract_qbo/bulk_extract_qbo.sh
@@ -3,7 +3,6 @@
 #SBATCH --partition=serc
 #SBATCH --time=01:00:00
 #SBATCH -n 10
-#SBATCH -N 1
 #SBATCH --mail-type=FAIL,TIME_LIMIT_90
 
 module load python/3.12
@@ -13,7 +12,7 @@ for i in {0..9}; do
     if [ -d $1/$idx ]
     then
         echo "working on " $1/$idx
-        srun -N 1 -n 1 python extract_qbo/extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 &
+        srun -n 1 python extract_qbo/extract_qbo.py $1/$idx $2 $3 --output-name "${idx}_QBO_${2}_${3}.nc" --latitude_range=5 &
     fi
 done
 wait
