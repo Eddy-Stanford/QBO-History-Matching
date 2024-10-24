eccc124efa0a52bc1b9d25f07fdb162718660f72
Robert King <robcking@stanford.edu>
2024-10-24 14:03:47 -0700

    changes for GNU MiMA

--- slurm_scripts/model_run.sh
+++ slurm_scripts/model_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mimarunforyears
-#SBATCH --mem=16G
-#SBATCH --nodes=1
-#SBATCH --time=60:00:00
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=40:00:00
 #SBATCH --partition=serc
 #SBATCH --output=%a.log
 #SBATCH -c 1
@@ -13,9 +12,16 @@ iterations=$1
 printf -v idx "%02d" $SLURM_ARRAY_TASK_ID
 cd $idx
 
-echo "Running for " $iterations " years"
+module purge
+module load python/3.12
+module load openmpi
+module load netcdf-c 
+module load netcdf-fortran
+module load gcc/14
 
-mimacontainer="${MIMAPATH:-/home/groups/aditis2/robcking/mima.sif}"
+echo "Running for " $iterations " years"
+mimaexec="${MIMAPATH:-/home/groups/aditis2/robcking/mima.x}"
+# mimacontainer="${MIMAPATH:-/home/groups/aditis2/robcking/mima.sif}"
 
 for ((i=1;i<=$iterations;i++))
 do
@@ -24,9 +30,9 @@ do
     for j in {1..5}
     do
         echo "attempt ${j}"
-        srun --ntasks $SLURM_NTASKS apptainer run $mimacontainer && break || sleep 15;
+        srun --ntasks $SLURM_NTASKS $mimaexec && break || sleep 15;
     done
-    apptainer exec $mimacontainer /opt/MiMA/build/mppnccombine.singularity -r atmos_daily_${i}.nc atmos_daily.nc.????
+    mppnccombine -r atmos_daily_${i}.nc atmos_daily.nc.????
     
     cp RESTART/*res* INPUT/
     [ ! -d restart_history/restart_$i ] && mkdir -p restart_history/restart_$i
