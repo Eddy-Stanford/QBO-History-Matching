4b8a91025da05e6e06ba0fa030afcd47b24773b7
Robert King <robcking@stanford.edu>
2025-10-22 16:45:53 -0700

    CES change decorrelation of chain

--- ces.py
+++ ces.py
@@ -129,8 +129,8 @@ def get_ad99_ces(
         coord_min=np.array(coord_min),
         coord_max=np.array(coord_max),
         coord_names=space.coord_labels,
-        N=100 * nruns + burn_in,
+        N=25 * nruns + burn_in,
         burn_in=burn_in,
         delta=delta,
     )
-    return ces[::100]
+    return ces[::25]
